17106c7fc6da7e1a6d3a4569f2e74f06af38f3d6
Teijo Laine <teijo@aropupu.fi>
2020-04-03 00:42:40 -0700

    Intercept types

--- index.ts
+++ index.ts
@@ -1,25 +1,71 @@
+function toString(obj: any) {
+  return JSON.stringify(obj, null, 2);
+}
+
 async function demoFunction<B extends {}, R extends {}>(params: {
   url: string;
   method: "POST" | "GET";
   body?: B;
 }): Promise<R> {
+  const returnValue = { call: `${params.method} ${params.url}` };
+  console.log(`function(${toString(arguments)}) => ${toString(returnValue)}`);
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(({ call: `${params.method} ${params.url}` } as unknown) as R);
+      resolve((returnValue as unknown) as R);
     }, 100);
   });
 }
 
-function wrap<T extends (...args: any) => Promise<any>>(fn: T) {
+interface InterceptReturn {
+  op: "intercept-return";
+}
+
+interface InterceptCall {
+  op: "intercept-call";
+}
+
+interface DelayCall {
+  op: "delay-call";
+}
+
+interface DelayReturn {
+  op: "delay-return";
+}
+
+type Interceptor = InterceptCall | InterceptReturn | DelayCall | DelayReturn;
+
+function wrap<T extends (...args: any) => Promise<any>>(
+  interceptor: Interceptor,
+  fn: T
+) {
   return (...args: any[]): Promise<ReturnType<T>> => {
+    switch (interceptor.op) {
+      case "delay-call":
+        return new Promise((resolve) => {
+          setTimeout(() => fn(...args).then(resolve), 100);
+        });
+      case "delay-return":
+        return new Promise((resolve) => {
+          fn(...args).then((returnValue) =>
+            setTimeout(() => resolve(returnValue), 100)
+          );
+        });
+      case "intercept-call":
+        return new Promise((resolve) => {
+          setTimeout(() => fn(...args, { intercept: true }).then(resolve), 100);
+        });
+      case "intercept-return":
+        return new Promise((resolve) => {
+          fn(...args).then((returnValue) =>
+            resolve({ ...returnValue, intercept: true })
+          );
+        });
+        break;
+    }
     return new Promise((resolve) => {
       setTimeout(() => fn(...args).then(resolve), 100);
     });
   };
-}
-
-function toString(obj: any) {
-  return JSON.stringify(obj, null, 2);
 }
 
 async function benchmark<T>(fn: () => Promise<T>) {
@@ -38,7 +84,7 @@ async function main() {
   });
 
   await benchmark(() => {
-    const wrapped = wrap(demoFunction);
+    const wrapped = wrap({ op: "intercept-call" }, demoFunction);
 
     return wrapped({ url: "localhost", method: "GET" });
   });
