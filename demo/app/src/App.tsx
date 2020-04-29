9957cfe0e6e41568a3fd6f1d2c4a6643ed0fec58
Teijo Laine <teijo@aropupu.fi>
2020-04-28 23:18:37 -0700

    Clean up the demo code

--- demo/app/src/App.tsx
+++ demo/app/src/App.tsx
@@ -2,238 +2,148 @@ import React from "react";
 import "./App.css";
 import { intercept } from "./InterceptorModal";
 
-interface Counter {
-  pending: boolean;
-  value: number;
-}
-
-interface ConcatenatedString {
-  pending: boolean;
-  value: string;
-}
-
 interface AppState {
-  counterN: Counter;
-  counterA: Counter;
-  counterB: Counter;
-  counterC: Counter;
-  counterD: Counter;
-  stringA: ConcatenatedString;
-  stringB: ConcatenatedString;
+  counterN: number;
+  counterA: number;
+  counterB: number;
+  counterC: number;
+  counterD: number;
+  stringA: string;
+  stringB: string;
+}
+
+interface ButtonState {
+  disabled: boolean;
+}
+
+interface ButtonProps<T> {
+  onChange: (value: T) => void;
+  onClick: () => Promise<T>;
+}
+
+class Button<T> extends React.Component<ButtonProps<T>, ButtonState> {
+  state = {
+    disabled: false,
+  };
+
+  render() {
+    return (
+      <button
+        disabled={this.state.disabled}
+        onClick={() => {
+          this.setState(
+            {
+              disabled: true,
+            },
+            () => {
+              this.props.onClick().then((value) => {
+                this.setState(
+                  {
+                    disabled: false,
+                  },
+                  () => {
+                    this.props.onChange(value);
+                  }
+                );
+              });
+            }
+          );
+        }}
+      >
+        {this.props.children}
+        {this.state.disabled && " (waiting)"}
+      </button>
+    );
+  }
 }
 
 class App extends React.Component<any, AppState> {
   state = {
-    counterN: { pending: false, value: 0 },
-    counterA: { pending: false, value: 0 },
-    counterB: { pending: false, value: 0 },
-    counterC: { pending: false, value: 0 },
-    counterD: { pending: false, value: 0 },
-    stringA: { pending: false, value: "" },
-    stringB: { pending: false, value: "" },
+    counterN: 0,
+    counterA: 0,
+    counterB: 0,
+    counterC: 0,
+    counterD: 0,
+    stringA: "",
+    stringB: "",
   };
 
   render() {
-    const disabledN = this.state.counterN.pending;
-    const disabledA = this.state.counterA.pending;
-    const disabledB = this.state.counterB.pending;
-    const disabledC = this.state.counterC.pending;
-    const disabledD = this.state.counterD.pending;
-    const disabledStringA = this.state.stringA.pending;
-    const disabledStringB = this.state.stringB.pending;
-
     return (
       <div className="App">
         <h1>Interceptor</h1>
         <h2>Single argument demo</h2>
         <div className="demo">
-          <div className="counter">{this.state.counterN.value}</div>
-          <button
-            disabled={disabledN}
-            onClick={() => {
-              this.setState(
-                {
-                  counterN: { ...this.state.counterN, pending: true },
-                },
-                () => {
-                  this.square(1).then((increment) => {
-                    this.setState({
-                      counterN: {
-                        pending: false,
-                        value: this.state.counterN.value + increment,
-                      },
-                    });
-                  });
-                }
-              );
+          <div className="counter">{this.state.counterN}</div>
+          <Button
+            onClick={() => this.square(1)}
+            onChange={(value) => {
+              this.setState({ counterN: this.state.counterN + value });
             }}
           >
-            += await square(1) => 1{disabledN && " (waiting)"}
-          </button>
-          <div className="counter">{this.state.counterA.value}</div>
-          <button
-            disabled={disabledA}
-            onClick={() => {
-              this.setState(
-                {
-                  counterA: { ...this.state.counterA, pending: true },
-                },
-                () => {
-                  intercept(
-                    this.square,
-                    "call"
-                  )(1).then((increment) => {
-                    this.setState({
-                      counterA: {
-                        pending: false,
-                        value: this.state.counterA.value + increment,
-                      },
-                    });
-                  });
-                }
-              );
+            += await square(1) => 1
+          </Button>
+          <div className="counter">{this.state.counterA}</div>
+          <Button
+            onClick={() => intercept(this.square, "call")(1)}
+            onChange={(value) => {
+              this.setState({ counterA: this.state.counterA + value });
             }}
           >
-            += await square(INTERCEPT(1)) => 1{disabledA && " (waiting)"}
-          </button>
-          <div className="counter">{this.state.counterB.value}</div>
-          <button
-            disabled={disabledB}
-            onClick={() => {
-              this.setState(
-                {
-                  counterB: { ...this.state.counterB, pending: true },
-                },
-                () => {
-                  intercept(
-                    this.square,
-                    "return"
-                  )(2).then((increment) => {
-                    this.setState({
-                      counterB: {
-                        pending: false,
-                        value: this.state.counterB.value + increment,
-                      },
-                    });
-                  });
-                }
-              );
+            += await square(INTERCEPT(1)) => 1
+          </Button>
+          <div className="counter">{this.state.counterB}</div>
+          <Button
+            onClick={() => intercept(this.square, "return")(2)}
+            onChange={(value) => {
+              this.setState({ counterB: this.state.counterB + value });
             }}
           >
-            += await square(2) => INTERCEPT(4){disabledB && " (waiting)"}
-          </button>
-          <div className="counter">{this.state.counterC.value}</div>
-          <button
-            disabled={disabledC}
-            onClick={() => {
-              this.setState(
-                {
-                  counterC: { ...this.state.counterC, pending: true },
-                },
-                () => {
-                  intercept(
-                    this.square,
-                    "both"
-                  )(3).then((increment) => {
-                    this.setState({
-                      counterC: {
-                        pending: false,
-                        value: this.state.counterC.value + increment,
-                      },
-                    });
-                  });
-                }
-              );
+            += await square(2) => INTERCEPT(4)
+          </Button>
+          <div className="counter">{this.state.counterC}</div>
+          <Button
+            onClick={() => intercept(this.square, "both")(3)}
+            onChange={(value) => {
+              this.setState({ counterC: this.state.counterC + value });
             }}
           >
             += await square(INTERCEPT(3)) => INTERCEPT(9)
-            {disabledC && " (waiting)"}
-          </button>
-          <div className="counter">{this.state.counterD.value}</div>
-          <button
-            disabled={disabledD}
-            onClick={() => {
-              this.setState(
-                {
-                  counterD: { ...this.state.counterD, pending: true },
-                },
-                () => {
-                  intercept(
-                    this.square,
-                    "bypass"
-                  )(4).then((increment) => {
-                    this.setState({
-                      counterD: {
-                        pending: false,
-                        value: this.state.counterD.value + increment,
-                      },
-                    });
-                  });
-                }
-              );
+          </Button>
+          <div className="counter">{this.state.counterD}</div>
+          <Button
+            onClick={() => intercept(this.square, "bypass")(4)}
+            onChange={(value) => {
+              this.setState({ counterD: this.state.counterD + value });
             }}
           >
             += await INTERCEPT(square(4)) => ???
-            {disabledD && " (waiting)"}
-          </button>
+          </Button>
         </div>
         <h2>Multiple arguments</h2>
         <div className="demo">
-          <div className="counter">{this.state.stringA.value}</div>
-          <button
-            disabled={disabledStringA}
-            onClick={() => {
-              this.setState(
-                {
-                  stringA: { ...this.state.stringA, pending: true },
-                },
-                () => {
-                  this.concat(this.state.stringA.value, "x", "y").then(
-                    (newString) => {
-                      this.setState({
-                        stringA: {
-                          pending: false,
-                          value: newString,
-                        },
-                      });
-                    }
-                  );
-                }
-              );
+          <div className="counter">{this.state.stringA}</div>
+          <Button
+            onClick={() => this.concat(this.state.stringA, "x", "y")}
+            onChange={(value) => {
+              this.setState({ stringA: value });
             }}
           >
-            = await concat("{this.state.stringA.value}", "x", "y") => "
-            {this.state.stringA.value}xy"
-            {disabledStringA && " (waiting)"}
-          </button>
-          <div className="counter">{this.state.stringB.value}</div>
-          <button
-            disabled={disabledStringB}
-            onClick={() => {
-              this.setState(
-                {
-                  stringB: { ...this.state.stringB, pending: true },
-                },
-                () => {
-                  intercept(this.concat, "call")(
-                    this.state.stringB.value,
-                    "x",
-                    "y"
-                  ).then((newString) => {
-                    this.setState({
-                      stringB: {
-                        pending: false,
-                        value: newString,
-                      },
-                    });
-                  });
-                }
-              );
+            = await concat("{this.state.stringA}", "x", "y") => "
+            {this.state.stringA}xy"
+          </Button>
+          <div className="counter">{this.state.stringB}</div>
+          <Button
+            onClick={() =>
+              intercept(this.concat, "call")(this.state.stringB, "x", "y")
+            }
+            onChange={(value) => {
+              this.setState({ stringB: value });
             }}
           >
-            = await concat(INTERCEPT("{this.state.stringB.value}", "x", "y")) =>
-            "{this.state.stringB.value}xy"{disabledStringB && " (waiting)"}
-          </button>
+            = aw= await concat(INTERCEPT("{this.state.stringB}", "x", "y")) => "
+            {this.state.stringB}xy"
+          </Button>
         </div>
       </div>
     );
